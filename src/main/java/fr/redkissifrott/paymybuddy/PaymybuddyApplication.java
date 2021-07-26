fd7128e11f12b7d2ec466c7c9214003725ca179f
RedKissiFrott <redkissifrott@yahoo.fr>
2021-07-26 11:06:55 +0200

    config

--- src/main/java/fr/redkissifrott/paymybuddy/PaymybuddyApplication.java
+++ src/main/java/fr/redkissifrott/paymybuddy/PaymybuddyApplication.java
@@ -3,6 +3,7 @@ package fr.redkissifrott.paymybuddy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 @SpringBootApplication
 public class PaymybuddyApplication {
 
